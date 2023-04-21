affcff7557316254ab2ee71ea72767a48903ad9b
DESKTOP-029KM01\123 <w18253555960@163.com>
2023-04-21 11:21:05 +0800

    未解决映射标注 line宽度变化问题

--- public/js/image.js
+++ public/js/image.js
@@ -305,8 +305,13 @@ class ImageContext extends MovableView {
     to_viewbox_coord(x, y) {
         var div = this.ui.querySelector("#maincanvas-svg");
 
-        x = Math.round(x * 2048 / div.clientWidth);
-        y = Math.round(y * 1536 / div.clientHeight);
+        /* x = Math.round(x * 2048 / div.clientWidth);
+        y = Math.round(y * 1536 / div.clientHeight); */
+        const imgSize = this.imageEditor.imgSize;
+
+        x = Math.round(x * imgSize.x / div.clientWidth);
+        y = Math.round(y * imgSize.y / div.clientHeight);
+
         return [x, y];
 
     }
@@ -330,16 +335,23 @@ class ImageContext extends MovableView {
         if (!img || img.width == 0) {
             return null;
         }
+        this.imageEditor.getImagePixelRatio(img.naturalWidth, img.naturalHeight);
 
         var clientWidth, clientHeight;
 
-        clientWidth = 2048;
-        clientHeight = 1536;
+        /* clientWidth = 2048;
+        clientHeight = 1536; */
+
+        const size = this.imageEditor.imgSize;
+
+        clientWidth = size.x;
+        clientHeight = size.y;
 
         var trans_ratio = {
             x: clientWidth / img.naturalWidth,
             y: clientHeight / img.naturalHeight,
         };
+
 
         return trans_ratio;
     }
@@ -349,6 +361,7 @@ class ImageContext extends MovableView {
 
         // active img is set by global, it's not set sometimes.
         var img = this.world.cameras.getImageByName(this.name);
+
         if (img) {
             svgimage.setAttribute("xlink:href", img.src);
         }
@@ -671,22 +684,33 @@ class ImageEditor {
     start = false; // 当前是否正在拖放添加矩形
     startDom = null; // 当前正在添加(鼠标未弹起)的矩形id
     rectData = null; // 鼠标坐标, 数组[x, y]
-    id_now_select = 'id_now_select_default'; // 当前选中的标注(2D/3D)
+    nowSelect_id = 'id_now_select_default'; // 当前选中的标注(2D/3D)
     topDot = null; // 拖拽模式下鼠标拖拽顶点的斜对顶点
-    obj_id_3d = null; // 当前受选3D标注的obj_id
+    objId_3d = null; // 当前受选3D标注的obj_id
     vector = null; // 当前向量
     ui = d3.select('#image-board');
+    basic_color = '#fff'
+    imgSize = {
+        x: 2048,
+        y: 1536
+    }
     annotation_2d = {
         obj_type: 'annotation_2d',
         psr: []
     }
-    basic_color = '#fff'
+
+    getImagePixelRatio(width, height) { // 没改svg的biewbox啊
+        this.imgSize.x = width;
+        this.imgSize.y = height;
+        this.ui.select('#maincanvas-svg').attr('viewBox', `0 0 ${width} ${height}`);
+        this.ui.select('#svg-image').attr('width', width).attr('height', height);
+    }
 
     getRate() {
-        const boardData = getDomInfo(document.querySelector('#image-board'));
+        const boardData = document.querySelector('#maincanvas-svg');
         const rates = {
-            rateX: 2048 / boardData.width,
-            rateY: 1536 / boardData.height
+            x: this.imgSize.x / boardData.clientWidth,
+            y: this.imgSize.y / boardData.clientHeight
         }
         return rates;
     }
@@ -722,8 +746,8 @@ class ImageEditor {
         // 星标
         const positionXY = svg.append('g').attr('class', 'line-g');
         svg.append('g').attr('id', 'rect-g'); // 标注组
-        positionXY.append('line').attr('id', 'line-x').attr('x1', 0).attr('y1', 0).attr('x2', 2048).attr('y2', 0).style('stroke', 'white').attr('stroke-width', 0);
-        positionXY.append('line').attr('id', 'line-y').attr('x1', 0).attr('y1', 0).attr('x2', 0).attr('y2', 1536).style('stroke', 'white').attr('stroke-width', 0);
+        positionXY.append('line').attr('id', 'line-x').attr('x1', 0).attr('y1', 0).attr('x2', this.imgSize.x).attr('y2', 0).style('stroke', 'white').attr('stroke-width', 0);
+        positionXY.append('line').attr('id', 'line-y').attr('x1', 0).attr('y1', 0).attr('x2', 0).attr('y2', this.imgSize.y).style('stroke', 'white').attr('stroke-width', 0);
         positionXY.append('circle').attr('id', 'line-circle').attr('cx', -10).attr('cy', -10).attr('r', 5).attr('fill', 'red');
 
         // 这个时刻没有world, 不能在此重应用
@@ -732,7 +756,7 @@ class ImageEditor {
 
     annotate_pic_set_id() {
         const id = this.ui.select('#header-id-list').property('value');
-        d3.select(`#${this.id_now_select}-rect`).attr(`obj_id`, id);
+        d3.select(`#${this.nowSelect_id}-rect`).attr(`obj_id`, id);
     }
 
     annotate_pic_clear() { // 清除全部标注
@@ -810,14 +834,14 @@ class ImageEditor {
             hide.style('display', 'none');
 
             d3.selectAll('#rect-g > g > circle').remove();
-            d3.select(`#${this.id_now_select}-rect`).style('stroke', this.basic_color).attr('fill', this.basic_color);
+            d3.select(`#${this.nowSelect_id}-rect`).style('stroke', this.basic_color).attr('fill', this.basic_color);
         }
         this.isDrag = !this.isDrag;
         this.annotate_pic_box_color();
     }
 
     annotate_pic_hide() { // 隐藏
-        const svg = d3.select(`#${this.id_now_select}`);
+        const svg = d3.select(`#${this.nowSelect_id}`);
         if (svg.style('visibility') === 'visible') {
             svg.style('visibility', 'hidden');
         }
@@ -829,16 +853,16 @@ class ImageEditor {
     }
 
     annotate_pic_delete() { // 删除
-        if (this.obj_id_3d !== null) { // 如果是3D转2D那么仅隐藏
-            const rect = this.get_box_by_obj_id(this.obj_id_3d);
-            if (rect.obj_track_id === this.obj_id_3d) {
+        if (this.objId_3d !== null) { // 如果是3D转2D那么仅隐藏
+            const rect = this.get_box_by_obj_id(this.objId_3d);
+            if (rect.obj_track_id === this.objId_3d) {
                 rect['draw'] = false;
             }
         }
-        const target_obj_id = this.get_obj_id_by_id(this.id_now_select);
+        const target_obj_id = this.get_obj_id_by_id(this.nowSelect_id);
         this.annotate_pic_remove_label(target_obj_id);
 
-        d3.select(`#${this.id_now_select}`).remove();
+        d3.select(`#${this.nowSelect_id}`).remove();
     }
 
     get_obj_id_by_id(id) { // 如果是3D转那么返回'3D'
@@ -851,7 +875,7 @@ class ImageEditor {
             res = d3.select(`#${id} > rect`).attr('obj_id');
             return res;
         }
-        if(id.substring(0, 14) === 'svg-box-local-') { // 
+        if(id.substring(0, 14) === 'svg-box-local-') {
             res = id.substring(14, id.length);
             return res;
         }
@@ -872,8 +896,8 @@ class ImageEditor {
     }
 
     annotate_pic_box_color() {
-        const allRect = d3.selectAll("#rect-g > g > rect")._groups[0]; // allRect没来得及变就直接执行了, 找不到对应id
-        for(let i = 0; i < allRect.length; i++) { // 需要判定一下, 如果在finish上色, 但是save上色不需要
+        const allRect = d3.selectAll("#rect-g > g > rect")._groups[0];
+        for(let i = 0; i < allRect.length; i++) {
             if(d3.select(allRect[i]).attr('obj_id')) {
                 d3.select(allRect[i])
                 .attr('fill', this.get_color_by_obj_id(d3.select(allRect[i]).attr('obj_id')))
@@ -937,20 +961,20 @@ class ImageEditor {
     }
 
     annotate_pic_after_switch() { // 受选标注切换后
-        if (this.id_now_select.substring(0, 7) === 'rect-g-') { // 若之前选中2D标注
-            d3.select(`#${this.id_now_select}-rect`).style('stroke', this.basic_color).attr('fill', this.basic_color);
-            d3.selectAll(`#${this.id_now_select} > circle`).attr('fill', this.basic_color);
+        if (this.nowSelect_id.substring(0, 7) === 'rect-g-') { // 若之前选中2D标注
+            d3.select(`#${this.nowSelect_id}-rect`).style('stroke', this.basic_color).attr('fill', this.basic_color);
+            d3.selectAll(`#${this.nowSelect_id} > circle`).attr('fill', this.basic_color);
             return;
         }
-        d3.select(`#${this.id_now_select}`).attr('fill', '');
+        d3.select(`#${this.nowSelect_id}`).attr('fill', '');
     }
 
     annotate_pic_select_3d(obj_local_id) {
         const boxes = this.world.annotation.boxes;
         const target = boxes.find((item) => {
-            return Number(item.obj_local_id) === Number(obj_local_id);
+            return Number(item.obj_local_id) === Number(obj_local_id); // box类型不稳定
         })
-        this.obj_id_3d = target.obj_track_id;
+        this.objId_3d = target.obj_track_id;
         this.ui.select('#header-id-list').property('value', target.obj_track_id);
     }
 
@@ -982,12 +1006,12 @@ class ImageEditor {
                 const fillColor = d3.select(this).style('stroke');
                 d3.select(this).attr('fill', fillColor);
 
-                if (d3.select(this).attr('id') !== that.id_now_select) {
+                if (d3.select(this).attr('id') !== that.nowSelect_id) {
                     that.annotate_pic_after_switch();
                 }
 
-                that.id_now_select = d3.select(this).attr('id');
-                let local_id = that.get_obj_id_by_id(that.id_now_select);
+                that.nowSelect_id = d3.select(this).attr('id');
+                let local_id = that.get_obj_id_by_id(that.nowSelect_id);
                 that.annotate_pic_select_3d(local_id);
             });
             return;
@@ -996,12 +1020,12 @@ class ImageEditor {
             if (that.isDrag === false) return;
 
             d3.select(this).style('stroke', 'red').attr('fill', 'red');
-            if (d3.select(this).attr('id') !== `${that.id_now_select}-rect`) {
+            if (d3.select(this).attr('id') !== `${that.nowSelect_id}-rect`) {
                 that.annotate_pic_after_switch();
             }
 
-            that.id_now_select = d3.select(this).attr('id').substring(0, d3.select(this).attr('id').length - 5);
-            d3.selectAll(`#${that.id_now_select} > circle`).attr('fill', 'red');
+            that.nowSelect_id = d3.select(this).attr('id').substring(0, d3.select(this).attr('id').length - 5);
+            d3.selectAll(`#${that.nowSelect_id} > circle`).attr('fill', 'red');
 
             let obj_id = d3.select(this).attr('obj_id') === null ? `` : d3.select(this).attr('obj_id');
 
@@ -1014,7 +1038,7 @@ class ImageEditor {
             const that = this;
             const id = new Date().getTime() + '';
 
-            const xy = [(+e.offsetX) * this.getRate().rateX, (+e.offsetY) * this.getRate().rateY];
+            const xy = [(+e.offsetX) * this.getRate().x, (+e.offsetY) * this.getRate().y];
 
             const rect = d3.select('#rect-g')
                 .append('g')
@@ -1039,14 +1063,14 @@ class ImageEditor {
     annotate_pic_mouse_move(e) {
         if (!this.isDrag) {
             const xy = [(+e.offsetX), (+e.offsetY)];
-            const xy0 = xy[0] * this.getRate().rateX;
-            const xy1 = xy[1] * this.getRate().rateY;
+            const xy0 = xy[0] * this.getRate().x;
+            const xy1 = xy[1] * this.getRate().y;
 
             // 更新星标
             d3.select('#line-x')
                 .attr('x1', 0)
                 .attr('y1', xy1)
-                .attr('x2', 2048)
+                .attr('x2', this.imgSize.x)
                 .attr('y2', xy1)
                 .style('stroke', 'white')
                 .attr('stroke-width', 2);
@@ -1055,7 +1079,7 @@ class ImageEditor {
                 .attr('x1', xy0)
                 .attr('y1', 0)
                 .attr('x2', xy0)
-                .attr('y2', 1536)
+                .attr('y2', this.imgSize.y)
                 .style('stroke', 'white')
                 .attr('stroke-width', 2);
 
@@ -1135,8 +1159,8 @@ class ImageEditor {
             })
             .on('drag', function (e) {
                 const dot = [
-                    (+e.sourceEvent.offsetX) * that.getRate().rateX,
-                    (+e.sourceEvent.offsetY) * that.getRate().rateY
+                    (+e.sourceEvent.offsetX) * that.getRate().x,
+                    (+e.sourceEvent.offsetY) * that.getRate().y
                 ] // 获取点坐标
                 if (widget._groups[0][0].localName === 'circle') {
                     // 判断新点相对于旧点的信息, 获取新点坐标
@@ -1185,8 +1209,8 @@ class ImageEditor {
         for (let i = 0; i < rectAll.length; i++) {
             if (d3.select(rectAll[i]).attr('obj_id') === obj_id) {
                 return {
-                    x: (Number(d3.select(rectAll[i]).attr('x')) + Number(d3.select(rectAll[i]).attr('width'))) / this.getRate().rateX,
-                    y: Number(d3.select(rectAll[i]).attr('y')) / this.getRate().rateY
+                    x: (Number(d3.select(rectAll[i]).attr('x')) + Number(d3.select(rectAll[i]).attr('width'))) / this.getRate().x,
+                    y: Number(d3.select(rectAll[i]).attr('y')) / this.getRate().y
                 };
             }
         }
